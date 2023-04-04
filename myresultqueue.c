203a2b0ca0f84d60bc14ce4736fe1081336b89e5
Ana Pacheco <121977567+anaspacheco@users.noreply.github.com>
2023-04-04 12:50:28 -0400

    Add files via upload

---
+++ myresultqueue.c
@@ -0,0 +1,61 @@
+#include "myresultqueue.h"
+#include <stdlib.h>
+
+//Implementation source: https://www.youtube.com/watch?v=FMNnusHqjpw
+
+void init_queue_result(ResultQueue *queue){
+    queue->head = NULL;
+    queue->tail = NULL;
+    queue->result_num = 0;
+    queue->done = 0;
+    pthread_mutex_init(&queue->mutex, NULL);
+    pthread_cond_init(&queue->empty, NULL);
+}
+
+void enqueue_result(ResultQueue *queue, char* character, int result_id, int size){
+    Result *result = malloc(sizeof(Result));
+    result->encoded = character;
+    result->result_id = result_id;
+    result->size = size;
+    result->next = NULL;
+
+    pthread_mutex_lock(&queue->mutex); // Mutex lock so that we can add the task to the queue without a race 
+    if(queue->head != NULL){
+        queue->tail->next =result;
+        queue->tail = result;
+    } else{
+        queue-> head = result; queue -> tail = result;
+    }
+    queue->result_num++;
+
+    pthread_mutex_unlock(&queue->mutex); // Unlock mutex so that other producers can access the queue 
+    pthread_cond_signal(&queue->empty); // Signal to consumers that the queue is not empty anymore 
+}
+
+Result* dequeue_result(ResultQueue *queue){
+    pthread_mutex_lock(&queue->mutex); // Mutex lock so that we can delete the task of the queue without a race 
+    while(queue->head == NULL && queue->done != 1){
+        pthread_cond_wait(&queue->empty, &queue->mutex); //Threads should wait until there is a task to delete the task. Also, it unblocks
+        //the queue in case any producer wants to use it, avoiding a deadlock
+    }
+    if(queue->head == NULL && queue->done == 1){
+        pthread_mutex_unlock(&queue->mutex);
+        return NULL;
+    } 
+        Result *result = queue->head;
+        queue->head = queue->head->next;
+        if(queue->head == NULL){
+            queue->tail = NULL;
+        }
+        queue->result_num--;
+        pthread_mutex_unlock(&queue->mutex);
+        return result;
+    
+}
+
+void free_result_queue(ResultQueue *queue){
+    pthread_mutex_destroy(&queue->mutex);
+    pthread_cond_destroy(&queue->empty);
+}
+
+
--- nyuenc.c
+++ nyuenc.c
@@ -1,65 +1,251 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
-#define handle_error(msg) \
-    do { perror(msg); exit(EXIT_FAILURE); } while (0)
+#include <pthread.h>
+#include "mytaskqueue.h"
+#include <string.h>
+#include "myresultqueue.h"
+#include <stdbool.h>
 
-int main(int argc, char* argv[]){
+#define handle_error(msg)   \
+    do                      \
+    {                       \
+        perror(msg);        \
+        exit(EXIT_FAILURE); \
+    } while (0)
+
+TaskQueue task_queue;
+ResultQueue result_queue;
+
+int task_id = 0;
+
+int total_results = 0;
+
+void *encoder_multithreaded()
+{
+    while(1){
+        Task *task = dequeue_task(&task_queue);
+        if(task == NULL){
+            pthread_exit(NULL);
+        } else{
+            int size = 0;
+            char* encoded = malloc(sizeof(char) * 2000);
+            char* addr = task->chunk_address;
+            unsigned int char_count = 0;
+            int index = 0;
+            char prev_char = addr[0];
+            for(int i = 0; i < task->chunk_size; i++){
+                if(addr[i] != prev_char){
+                   encoded[index++] = prev_char;
+                   encoded[index++] = char_count;
+                   size+=2;
+                   //printf("%d", char_count);
+                   prev_char = addr[i];
+                   //printf("%c", prev_char);
+                   char_count = 1;
+                } else{
+                    char_count++;
+                }
+            }
+            encoded[index++] = prev_char;
+            encoded[index++] = char_count;
+            encoded[index] = '\0';
+            size+=2;
+            enqueue_result(&result_queue,encoded, task_id, size);
+            pthread_cond_signal(&result_queue.empty);
+        }
+        } 
+        return NULL;
+    }
+
+// Multi-threaded encoder
+int main(int argc, char *argv[])
+{
+
     int output = STDOUT_FILENO;
-   
+
+    // Getting the number of threads https://linux.die.net/man/3/getopt
+    int thread_count = 1;
+    int opt;
+    while ((opt = getopt(argc, argv, "j:")) != -1)
+    {
+        switch (opt)
+        {
+        case 'j':
+            thread_count = atoi(optarg);
+            break;
+        case '?':
+            handle_error("opt");
+        }
+    }
+    // Initiliaze result queue
+    init_queue_task(&task_queue);
+    init_queue_result(&result_queue);
+
+    
+    pthread_t *threads = malloc(thread_count * sizeof(pthread_t));
+
+    for (int i = 0; i < thread_count; i++)
+    {
+        if (pthread_create(&threads[i], NULL, encoder_multithreaded, NULL) != 0)
+        {
+            handle_error("pthread_create");
+        }
+    }
+
     char last_char = 0;
     int last_count = 0;
-
-    //Looping through all input files (skipping the program)
-    for(int i = 1; i < argc; i++){
+    // Looping through all input files
+    for (int i = optind; i < argc; i++)
+    {
         int fd = open(argv[i], O_RDONLY);
-        if (fd == -1){
+        if (fd == -1)
+        {
             handle_error("fd");
         }
 
         // Get file size
         struct stat sb;
-        if (fstat(fd, &sb) == -1){
+        if (fstat(fd, &sb) == -1)
+        {
             handle_error("sb");
         }
 
         // Map file into memory
         char *addr = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
-        if (addr == MAP_FAILED){
+        if (addr == MAP_FAILED)
+        {
             handle_error("addr");
         }
 
-        //Checking the # of occurances of each char and writing it to stdout
-        int char_count = 1;
-        char prev_char = addr[0];
+        if (thread_count > 1)
+        {
 
-        if(prev_char == last_char && i > 1){
-            char_count+=last_count;
-        }
-        for(int j = 1; j < sb.st_size; j++){
-            if(addr[j] != prev_char){
-                write(output, &prev_char, 1);
-                write(output, &char_count, 1);
-                char_count = 1;
-                prev_char= addr[j];
-            } else{
-                char_count++;
+            int chunk_size = 4096; // 4KB
+
+            int chunk_count = sb.st_size / chunk_size + (sb.st_size % chunk_size != 0);
+            total_results += chunk_count;
+            // printf("%d \n", chunk_count);
+
+            char *mem_address;
+
+            for (int j = 0; j < chunk_count; j++)
+            {
+                if (j == chunk_count - 1)
+                {
+                    chunk_size = sb.st_size - j * chunk_size;
+                }
+                mem_address = addr + j * chunk_size;
+                enqueue_task(&task_queue, mem_address, chunk_size, task_id++);
             }
         }
-        last_char = prev_char; last_count = char_count;
-        if(i == argc - 1) {
-            write(output, &prev_char, 1);
-            write(output, &char_count, 1);
+        else
+        {
+            // Checking the # of occurances of each char and writing it to stdout
+            int char_count = 1;
+            char prev_char = addr[0];
+
+            if (prev_char == last_char && i > 1)
+            {
+                char_count += last_count;
+            }
+            for (int j = 1; j < sb.st_size; j++)
+            {
+                if (addr[j] != prev_char)
+                {
+                    write(output, &prev_char, 1);
+                    write(output, &char_count, 1);
+                    char_count = 1;
+                    prev_char = addr[j];
+                }
+                else
+                {
+                    char_count++;
+                }
+            }
+            last_char = prev_char;
+            last_count = char_count;
+            if (i == argc - 1)
+            {
+                write(output, &prev_char, 1);
+                write(output, &char_count, 1);
+            }
+            // Closing fd and freeing memory https://linux.die.net/man/2/munmap
+            close(fd);
+            munmap(addr, sb.st_size);
         }
-        //Closing fd and freeing memory https://linux.die.net/man/2/munmap 
-        close(fd);
-        munmap(addr, sb.st_size);
     }
+    if (thread_count > 1)
+        { 
+            task_queue.done = 1;            
+            pthread_cond_broadcast(&task_queue.empty);
+
+
+            int processed_results = 0;
+            /*
+             Result **all_results = malloc(sizeof(Result) * 5000);
+            int result_index = 1;
+            int index = 0;
+            char last_char;
+            unsigned int last_count = 0;
+            */
+            bool exit_flag = false;
+            
+            while(1){
+                if(processed_results == total_results){
+                    result_queue.done = 1;
+                }
+                Result *result = dequeue_result(&result_queue); 
+                    if(result == NULL){
+                        break;
+                        //goto exit_loop;
+                     } else{
+                        //printf("%d", result->result_id);
+                    }
+                processed_results++;
+                /*
+                while(all_results[result_index] == NULL){
+                     Result *result = dequeue_result(&result_queue); 
+                     if(result == NULL){
+                        goto exit_loop;
+                     } else{
+                        index = result->result_id;
+                        all_results[index] = result;
+                    }
+                }
+                Result *to_process = all_results[result_index];
+                char* encoded = to_process->encoded;
+                int len = to_process->size;
+                if(index == 1){
+                    last_char = encoded[len - 2];
+                    last_count = encoded[len - 1];
+                    write(output, encoded, len - 1);
+                } else{
+                    if(encoded[len - 2] == last_char){
+                        unsigned int sum = last_count + encoded[len - 1];
+                        write(output, &sum, 1);
+                        write(output, encoded + 2, len - 3);
+                    } else{
+                        write(output, encoded, len - 1);
+                    }
+                    last_char = encoded[len - 2];
+                    last_count = encoded[len - 1];
+                    if(index == total_results){
+                        write(output, &last_count, 1);
+                    }
+                }
+                result_index++;
+                */
+            }
+            //exit_loop:
+
+            for(int i = 0; i < thread_count; i++){
+                pthread_join(threads[i], NULL);
+            }
+            
+        }
 }
-
-
-
-
